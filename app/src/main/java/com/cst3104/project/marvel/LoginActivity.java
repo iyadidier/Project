3e902833de225f3dee23fdc24af88b2dc350c496
iyadidier <iyamuremyedidier20@gmail.com>
2024-11-19 17:00:48 -0500

    commit

---
+++ app/src/main/java/com/cst3104/project/marvel/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.cst3104.project.marvel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.cst3104.project.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);  // Make sure this is the correct layout
+
+        // Initialize the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);  // Ensure the toolbar has this ID in the XML
+        setSupportActionBar(toolbar);  // Set the toolbar as the action bar
+
+        // Optional: Set a title on the toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Login");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle menu item clicks
+        if (item.getItemId() == R.id.action_info) {
+            showInfoDialog();  // Show info dialog when the menu item is clicked
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showInfoDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("About the App")
+                .setMessage("This is a collaborative project by Team XYZ.\n\nAuthors:\n- Didier\n- Teammates")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+}
